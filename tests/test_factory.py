11c5fbf0f1e89feed2fbc99959ff632a0c87b4fa
alniterentev <terentev.an@phystech.edu>
2019-05-09 23:18:57 +0300

    fixed unit tests

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -8,7 +8,7 @@ class TestFactory(unittest.TestCase):
         self.assertEqual(spider.race, "Enemy")
         self.assertEqual(spider.name, "Spider")
         self.assertEqual(spider.health, 5)
-        self.assertEqual(spider._damage, 2)
+        self.assertEqual(spider.damage, 2)
         self.assertEqual(spider.move.__doc__,
                          "move left or right to meet character or turn")
         self.assertEqual(spider.attack.__doc__,
@@ -19,7 +19,7 @@ class TestFactory(unittest.TestCase):
         self.assertEqual(archer.race, "Enemy")
         self.assertEqual(archer.name, "Archer")
         self.assertEqual(archer.health, 2)
-        self.assertEqual(archer._damage, 3)
+        self.assertEqual(archer.damage, 3)
         self.assertEqual(archer.move.__doc__, "no move")
         self.assertEqual(archer.attack.__doc__, "attack ranged")
 
@@ -28,7 +28,7 @@ class TestFactory(unittest.TestCase):
         self.assertEqual(bee.race, "Enemy")
         self.assertEqual(bee.name, "Bee")
         self.assertEqual(bee.health, 1)
-        self.assertEqual(bee._damage, 1)
+        self.assertEqual(bee.damage, 1)
         self.assertEqual(bee.move.__doc__,
                          "randomly until sees a wall or decides to turn")
         self.assertEqual(bee.attack.__doc__,
@@ -39,7 +39,7 @@ class TestFactory(unittest.TestCase):
         self.assertEqual(scorpion.race, "Enemy")
         self.assertEqual(scorpion.name, "Scorpion")
         self.assertEqual(scorpion.health, 4)
-        self.assertEqual(scorpion._damage, 1)
+        self.assertEqual(scorpion.damage, 1)
         self.assertEqual(scorpion.move.__doc__, "moves to the character")
         self.assertEqual(scorpion.attack.__doc__, "DOT debuffing enemy")
 
@@ -48,7 +48,7 @@ class TestFactory(unittest.TestCase):
         self.assertEqual(zombie.race, "Enemy")
         self.assertEqual(zombie.name, "Zombie")
         self.assertEqual(zombie.health, 3)
-        self.assertEqual(zombie._damage, 2)
+        self.assertEqual(zombie.damage, 2)
         self.assertEqual(zombie.move.__doc__, "go to character")
         self.assertEqual(zombie.attack.__doc__, "attack melee")
 
